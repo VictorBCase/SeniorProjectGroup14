2fee7043a8f9884ea78119fd08f72b73f5604ba2
VictorBCase <vwb6@case.edu>
2025-10-15 17:41:32 -0400

    Added groups + small changes

--- backend/src/main/java/com/example/backend/VirtualQueue.java
+++ backend/src/main/java/com/example/backend/VirtualQueue.java
@@ -1,64 +1,80 @@
 package com.example.backend;
-
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class VirtualQueue {
-    private Queue<User> queue = new LinkedList<>();
+    private Queue<Object> queue = new LinkedList<>(); //Changed to object to account for class user and group
 
-    //Check current queue size
-    public int size() {
-        return queue.size();
-    }
-
-
-    //Add user to queue
     public void joinQueue(User user) {
         queue.add(user);
         System.out.println(user.getName() + " joined the queue. Position: " + queue.size());
     }
 
-    //Remove next user from queue
-    public User serveNext() {
-        User next = queue.poll();
-        if (next != null) {
-            System.out.println("It is now " + next.getName() + "'s turn for the ride. ");
-        } 
-        else {
-            System.out.println("Queue is empty.");
-        }
-        return next;
+    public void joinQueue(Group group) {
+        queue.add(group);
+        System.out.println(group.getGroupName() + " (Group) joined the queue with members: " + group.memberNames());
     }
 
-    public boolean leaveQueue(User user) {
-        String userId = user.getId();
-        for(User people : queue){
-            if(people.getId().equals(userId)){
-                queue.remove(people);
-                System.out.println(("You have succesfully left the queue"));
-                return true;
+    //Remove current object in queue (user / group)
+    public void serveNext() {
+        Object next = queue.poll();
+        if (next == null) {
+            System.out.println("Queue is empty.");
+            return;
+        }
+
+        if (next instanceof User) { //User 
+            User user = (User) next;
+            System.out.println("Serving " + user.getName());
+        } else if (next instanceof Group) { //Group
+            Group group = (Group) next;
+            System.out.println("Serving group: " + group.getGroupName() + " (" + group.memberNames() + ")");
+        }
+    }
+
+    //Manually leave queue early
+    public boolean leaveQueue(String id) {
+        Iterator<Object> iterator = queue.iterator();
+        while (iterator.hasNext()) {
+            Object obj = iterator.next();
+
+            if (obj instanceof User) {
+                User user = (User) obj;
+                if (user.getId().equals(id)) {
+                    iterator.remove();
+                    System.out.println(user.getName() + " left the queue voluntarily.");
+                    return true;
+                }
+            } else if (obj instanceof Group) {
+                Group group = (Group) obj;
+                if (group.getGroupId().equals(id)) {
+                    iterator.remove();
+                    System.out.println(group.getGroupName() + " (Group) left the queue voluntarily.");
+                    return true;
+                }
             }
         }
-        System.out.println("You are not in a queue currently");
+        System.out.println("No user or group found with that ID.");
         return false;
     }
 
-
-    //View all users in the VQueue
+    //View contents of queue
     public void viewQueue() {
         if (queue.isEmpty()) {
-            System.out.println("The queue is empty.");
+            System.out.println("Queue is currently empty.");
             return;
         }
-        
-        System.out.println("Current queue:");
+
+        System.out.println("\nCurrent V-Queue:");
         int position = 1;
-        for (User user : queue) {
-            System.out.println(position + ". " + user.getName() + " (ID: " + user.getId() + ")");
+        for (Object obj : queue) {
+            if (obj instanceof User) {
+                User user = (User) obj;
+                System.out.println(position + ". " + user.getName() + " (User ID: " + user.getId() + ")");
+            } else if (obj instanceof Group) {
+                Group group = (Group) obj;
+                System.out.println(position + ". [Group: " + group.getGroupName() + "] Members: " + group.memberNames());
+            }
             position++;
         }
     }
 }
-
-
-
