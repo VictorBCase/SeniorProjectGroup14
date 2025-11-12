6a9bd704429d4cc7b65293740bd3972ab66c8eff
VictorBCase <vwb6@case.edu>
2025-11-11 21:00:31 -0500

    user login with password hash

--- backend/src/main/java/com/example/backend/User.java
+++ backend/src/main/java/com/example/backend/User.java
@@ -4,20 +4,22 @@ import java.util.UUID;
 
 public class User {
     private String id;
-    private String name;
+    private String username;
+    private String passwordHash;
 
     //com.example.backend.User Constructor
     //Note, adding ID to constructor for unique identifcation
-    public User(String name) {
+    public User(String name, String passwordHash) {
         this.id = UUID.randomUUID().toString(); //UUID converted to string; can change if we don't want UUID
-        this.name = name;
+        this.username = name;
+        this.passwordHash = passwordHash;
     }
 
-    public String getId() {
-        return id;
-    }
+    public String getId() { return id; }
 
-    public String getName() {
-        return name;
-    }
+    public String getUsername() {return username;}
+
+    public String getPasswordHash() {return passwordHash;}
+
+
 }
