d2fafd6fe496c3520843d71d206333f994859479
VictorBCase <vwb6@case.edu>
2025-11-11 21:05:08 -0500

    Revert "user login with password hash"
    This reverts commit 6a9bd704429d4cc7b65293740bd3972ab66c8eff.

--- backend/src/main/java/com/example/backend/User.java
+++ backend/src/main/java/com/example/backend/User.java
@@ -4,22 +4,20 @@ import java.util.UUID;
 
 public class User {
     private String id;
-    private String username;
-    private String passwordHash;
+    private String name;
 
     //com.example.backend.User Constructor
     //Note, adding ID to constructor for unique identifcation
-    public User(String name, String passwordHash) {
+    public User(String name) {
         this.id = UUID.randomUUID().toString(); //UUID converted to string; can change if we don't want UUID
-        this.username = name;
-        this.passwordHash = passwordHash;
+        this.name = name;
     }
 
-    public String getId() { return id; }
+    public String getId() {
+        return id;
+    }
 
-    public String getUsername() {return username;}
-
-    public String getPasswordHash() {return passwordHash;}
-
-
+    public String getName() {
+        return name;
+    }
 }
