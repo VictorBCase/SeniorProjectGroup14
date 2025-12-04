845913963953172246d5798ccf4e6b28e9b8f627
dborhegyi <44074532+dborhegyi@users.noreply.github.com>
2025-12-04 01:21:25 -0500

    buttons work

--- app/src/main/java/com/example/seniorprojectgroup14/RiderStatusActivity.java
+++ app/src/main/java/com/example/seniorprojectgroup14/RiderStatusActivity.java
@@ -1,7 +1,9 @@
 package com.example.seniorprojectgroup14;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RiderStatusActivity extends Activity {
 
@@ -9,5 +11,20 @@ public class RiderStatusActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.rider_status_layout);
+    }
+
+    public void buttonClicked(View view) {
+        if (view.getId() == R.id.rideSolo) {
+            Intent intent = new Intent(RiderStatusActivity.this, QRScannerActivity.class);
+            startActivity(intent);
+        }
+        else if (view.getId() == R.id.createGroup) {
+            Intent intent = new Intent(RiderStatusActivity.this, CreateGroupActivity.class);
+            startActivity(intent);
+        }
+        else if (view.getId() == R.id.joinGroup) {
+            Intent intent = new Intent(RiderStatusActivity.this, JoinGroupActivity.class);
+            startActivity(intent);
+        }
     }
 }
